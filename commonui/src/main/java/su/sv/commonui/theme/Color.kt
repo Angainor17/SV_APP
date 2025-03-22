13cf1a6b24daaa10c3cde89936cf58f43bc6567d
angai <angainor17@gmail.com>
2025-03-23 00:46:36 +0300

    theme fix

--- commonui/src/main/java/su/sv/commonui/theme/Color.kt
+++ commonui/src/main/java/su/sv/commonui/theme/Color.kt
@@ -2,12 +2,18 @@ package su.sv.commonui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Dark
 val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val Grey80 = Color(0xFFB2ACB2)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
+val PurpleGrey40 = Color(0x66448AFF) // tertiaryContainer
+
+// Light
+val DarkBlue40 = Color(0xFF3F51B5)
 val Pink40 = Color(0xFF7D5260)
+val Grey40 = Color(0xFFCAC7DC)
 
-val Grey = Color(0xB3A81B1B)
+val PurpleGrey80 = Color(0xFF285191) // tertiaryContainer
+
+val White = Color(0xFFFFFFFF) // onTertiary
--- news/src/main/java/su/sv/news/presentation/root/RootNews.kt
+++ news/src/main/java/su/sv/news/presentation/root/RootNews.kt
@@ -1,11 +1,22 @@
 package su.sv.news.presentation.root
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
 @Composable
-fun RootNews(navController: NavHostController, viewModel : RootNewsViewModel = hiltViewModel()) {
-    Text("RootNews")
+fun RootNews(navController: NavHostController, viewModel: RootNewsViewModel = hiltViewModel()) {
+    Box(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize()
+    ) {
+        Text("RootNews")
+    }
 }
