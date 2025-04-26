b55c0c1f7b72085ca51dc9753df5f738e95b080f
angai <angainor17@gmail.com>
2025-04-26 19:37:02 +0300

    нормально отображение ошибки

--- commonui/src/main/java/su/sv/commonui/ui/FullScreenError.kt
+++ commonui/src/main/java/su/sv/commonui/ui/FullScreenError.kt
@@ -1,11 +1,13 @@
 package su.sv.commonui.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -14,7 +16,9 @@ import su.sv.commonui.R
 @Composable
 fun FullScreenError(refreshAction: () -> Unit) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
             text = stringResource(R.string.common_error_title),
