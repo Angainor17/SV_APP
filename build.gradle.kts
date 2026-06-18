e01064a8aafe7018aa94fc2a0c001cc0fc0be30e
Voronin Igor <i.u.voronin@vkteam.ru>
2026-06-18 12:26:54 +0300

    detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,36 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    toolVersion = libs.versions.detekt.get()
+    config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    source.setFrom(
+        files(
+            "app/src/main/java",
+            "commonarchitecture/src/main/java",
+            "commonui/src/main/java",
+            "main/src/main/java",
+            "books/src/main/java",
+            "wiki/src/main/java",
+            "news/src/main/java",
+            "info/src/main/java",
+            "models/src/main/java",
+            "api/src/main/java",
+            "managers/src/main/java",
+        )
+    )
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
 }
